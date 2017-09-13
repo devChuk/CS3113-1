d235b7c251480e30dd79cdf88fcbf63221ccaf82
Ivan Safrin <ivansafrin@gmail.com>
2017-09-13 02:05:53 -0400

    Fixed template file in Xcode project

--- project_template/Xcode/NYUCodebase/main.cpp
+++ project_template/Xcode/NYUCodebase/main.cpp
@@ -1,114 +1,40 @@
 #ifdef _WINDOWS
-#include <GL/glew.h>
+	#include <GL/glew.h>
 #endif
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
-#include "ShaderProgram.h"
-#include "Matrix.h"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
 
 #ifdef _WINDOWS
-#define RESOURCE_FOLDER ""
+	#define RESOURCE_FOLDER ""
 #else
-#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+	#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
 #endif
 
 SDL_Window* displayWindow;
 
-GLuint LoadTexture(const char *filePath) {
-	int w,h,comp;
-	unsigned char* image = stbi_load(filePath, &w, &h, &comp, STBI_rgb_alpha);
-	
-	if(image == NULL) {
-		std::cout << "Unable to load image. Make sure the path is correct\n";
-		assert(false);
-	}
-	
-	GLuint retTexture;
-	glGenTextures(1, &retTexture);
-	glBindTexture(GL_TEXTURE_2D, retTexture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	
-	stbi_image_free(image);
-	return retTexture;
-}
-
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
-    displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
-    SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
-    SDL_GL_MakeCurrent(displayWindow, context);
-#ifdef _WINDOWS
-    glewInit();
-#endif
-	
+	SDL_Init(SDL_INIT_VIDEO);
+	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+	SDL_GL_MakeCurrent(displayWindow, context);
+	#ifdef _WINDOWS
+		glewInit();
+	#endif
 
-	glViewport(0, 0, 640, 360);
-	
-	ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
-	
-	GLuint emojiTexture = LoadTexture(RESOURCE_FOLDER"emoji.png");
-	
-	Matrix projectionMatrix;
-	projectionMatrix.SetOrthoProjection(-3.55f, 3.55f, -2.0f, 2.0f, -1.0f, 1.0f);
-	Matrix modelviewMatrix;
-	
-	float lastFrameTicks = 0.0f;
-	
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	
-    SDL_Event event;
-    bool done = false;
-    while (!done) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
-                done = true;
-            }
-        }
-		
-		float ticks = (float)SDL_GetTicks()/1000.0f;
-		float elapsed = ticks - lastFrameTicks;
-		lastFrameTicks = ticks;
-		
-		glClearColor(0.5f, 1.0f, 0.3f, 1.0f);
+	SDL_Event event;
+	bool done = false;
+	while (!done) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+				done = true;
+			}
+		}
 		glClear(GL_COLOR_BUFFER_BIT);
-		
-		glUseProgram(program.programID);
-		
-		//modelviewMatrix.Identity();
-		modelviewMatrix.Rotate(elapsed);
-		
-		program.SetModelviewMatrix(modelviewMatrix);
-		program.SetProjectionMatrix(projectionMatrix);
-		
-		glBindTexture(GL_TEXTURE_2D, emojiTexture);
-		
-		float vertices[] = {-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5};
-		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
-		glEnableVertexAttribArray(program.positionAttribute);
-		
-		float texCoords[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
-		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
-		glEnableVertexAttribArray(program.texCoordAttribute);
-		
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		
-		glDisableVertexAttribArray(program.positionAttribute);
-		glDisableVertexAttribArray(program.texCoordAttribute);
-		
 		SDL_GL_SwapWindow(displayWindow);
 	}
-	
-    SDL_Quit();
-    return 0;
+
+	SDL_Quit();
+	return 0;
 }
